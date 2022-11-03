bece79fc481928847fbc457a1f0ada4f2d735bc2
dalian tillier <daliandu62@hotmail.fr>
2022-11-03 16:50:45 +0100

    Commit 5 : ajout categ dans l’accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,38 +14,63 @@ class HomeController extends AbstractController
 {
 
     private $repoArticle;
+    private $repoCategory;
 
-    public function __construct(ArticleRepository $repoArticle)
+    public function __construct(ArticleRepository $repoArticle, CategoryRepository $repoCategory)
     {
         $this->repoArticle = $repoArticle;
+        $this->repoCategory = $repoCategory;
     }
 
     /**
      * @Route("/route-param/{nom}", name="route_param")
      */
-public function routeParam(String $nom): Response
- {
- return $this->render('test/route-param.html.twig', [
- "nom"=>$nom
- ]);
+    public function routeParam(String $nom): Response
+    {
+        return $this->render('test/route-param.html.twig', [
+            "nom" => $nom
+        ]);
+    }
 
- }
+
     /**
-     * @Route("/", name="home")
+     * @Route("/}", name="home")
      */
-  public function index(): Response
-  {
-    $articles = $this->repoArticle->findAll();
-    return $this->render('home/index.html.twig', [
-      "articles" => $articles
-    ]);
-  }
+    public function index(CategoryRepository $repoCategory): Response
+    {
+        $categories = $repoCategory->findAll();
+        $articles = array_reverse($this->repoArticle->findAll());
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles,
+            "categories" => $categories,
+            "titre" => "Liste des articles"
+        ]);
+    }
+
+    /**
+     * @Route("/showArticles/{id}", name="show_articles")
+     */
+    public function showArticles(Category $category): Response
+    {
+        $categories = $this->repoCategory->findAll();
+
+        if ($category) {
+            $articles = $category->getArticles()->getValues();
+        } else {
+            return $this->redirectToRoute("home");
+        }
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles,
+            "categories" => $categories,
+            "titre" => $category->getTitle()
+        ]);
+    }
 
 
     /**
      * @Route("/show/{id}", name="show")
      */
-    public function show(Article $article =null): Response
+    public function show(Article $article = null): Response
     {
         if (!$article) {
             return $this->redirectToRoute("home");
